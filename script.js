02c62cebfd819922c0b64f35385b5bf908a86e9c
edgardjustino <eddiejustino@gmail.com>
2023-04-10 14:40:06 -0300

    last touchs on js

--- script.js
+++ script.js
@@ -2,7 +2,21 @@
 // (celsius x 1,8) + 32
 // (fah - 32)/1,8
 
-const celsius = document.querySelector('#input-celsius').value;
-const fahrenheit = document.querySelector('#input-fah').value;
+const celsius = document.querySelector('#input-celsius');
+const fahrenheit = document.querySelector('#input-fah');
 
 window.addEventListener('load', ()=> celsius.focus());
+
+celsius.addEventListener('input', ()=> {
+    fahrenheit.value = ((celsius.value * 9)/5 + 32).toFixed(2);
+    if (!celsius.value){
+        fahrenheit.value = '';
+    }
+})
+
+fahrenheit.addEventListener('input', ()=>{
+    celsius.value = (((fahrenheit.value - 32)*5/9)).toFixed(2);
+    if (!fahrenheit.value){
+        celsius.value = '';
+    }
+})
